db0ac12fab55daaa05c89a979a821c267da7bd1c
Zhuang, Jia Jun <kk69347321@gmail.com>
2024-10-05 19:00:50 +0800

    chore: rm solver dir

--- solver/node/utils.js
+++
@@ -1,8 +0,0 @@
-import { promises as fs } from 'fs';
-
-// 讀取輸入圖像並轉換為 base64
-export async function pathToImageBase64(imagePath) {
-    const inputImageBuffer = await fs.readFile(imagePath);
-    const inputBase64 = inputImageBuffer.toString('base64');
-    return inputBase64;
-}
