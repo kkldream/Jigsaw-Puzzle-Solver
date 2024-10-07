702e2dba3ab553f6ce8315c586c5b771e0db97f2
Zhuang, Jia Jun <kk69347321@gmail.com>
2024-10-07 20:39:02 +0800

    feat: LoginModel

--- web/src/app/_components/ui/LoginModel.tsx
+++ web/src/app/_components/ui/LoginModel.tsx
@@ -1,41 +1,31 @@
-import {ChangeEvent, Fragment, useMemo, useState} from 'react'
+import {Fragment, useEffect, useMemo, useState} from 'react'
 import {Dialog, Transition} from '@headlessui/react'
-import {imageFileToBase64Url} from "@/service/base64Service";
 import {useRouter} from 'next/navigation'
-import {ImageViewer} from "@/app/_components/ImageViewer";
-import api from "@/service/apiService";
 import LoadingSpinner from "@/app/_components/LoadingSpinner";
 
-export default function NewProjectModel(props: {
+export default function LoginModel(props: {
     open: boolean;
     setOpen: (value: (((prevState: boolean) => boolean) | boolean)) => void;
 }) {
-    const router = useRouter();
-    const [projectName, setProjectName] = useState<string>("");
-    const [imageFile, setImageFile] = useState<File | null>(null);
-    const [uploadLoading, setUploadLoading] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
 
-    const checkCanUpload = projectName !== "" && imageFile !== null;
+    const checkInputOk = username !== "" && password !== "";
 
-    function handleInputFile(e: ChangeEvent<HTMLInputElement>) {
-        if (e.target.files && e.target.files[0])
-            setImageFile(e.target.files[0]);
-    }
+    useEffect(() => {
+        setUsername("");
+        setPassword("");
+    }, [props.open]);
 
     async function handleButtonClick(isOk: boolean) {
-        setUploadLoading(true);
+        setLoading(true);
         if (isOk) {
-            if (!checkCanUpload) throw new Error("Invalid input");
-            const res = await api.project.POST(projectName, await imageFileToBase64Url(imageFile as File));
-            if (res.success) {
-                router.push(`/project/${res.result.projectId}`);
-                return;
-            }
+            if (!checkInputOk) throw new Error("Invalid input");
+            await new Promise((resolve) => setTimeout(resolve, 1000));
         }
         props.setOpen(false);
-        setProjectName("");
-        setImageFile(null);
-        setUploadLoading(false);
+        setLoading(false);
     }
 
     return (
@@ -70,76 +60,71 @@ export default function NewProjectModel(props: {
                                     <div className="text-center">
                                         <Dialog.Title as="h3"
                                                       className="text-base font-semibold leading-6 text-gray-900">
-                                            建立新的專案
+                                            登入
                                         </Dialog.Title>
-                                        <div className="mt-2">
+                                        <div className="mt-8">
                                             <div className="relative">
                                                 <label
                                                     htmlFor="name"
                                                     className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
                                                 >
-                                                    專案名稱
+                                                    帳號
                                                 </label>
                                                 <input
                                                     type="text"
                                                     name="name"
                                                     id="name"
                                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                    onChange={(e) => setProjectName(e.target.value)}
+                                                    onChange={(e) => setUsername(e.target.value)}
                                                 />
                                             </div>
                                         </div>
                                         <div className="mt-2">
                                             <p className="text-sm text-gray-500">
-                                                為你的專案取一個獨特的名稱，以方便後續能夠快速找到你的專案。
+                                                Julo Julo Julo Julo Julo Julo Julo。
                                             </p>
                                         </div>
-                                        <div className="mt-3">
-                                            <div className="relative block w-full h-full">
+                                        <div className="mt-8">
+                                            <div className="relative">
                                                 <label
-                                                    htmlFor="file-upload"
-                                                    className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 cursor-pointer"
+                                                    htmlFor="name"
+                                                    className="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
                                                 >
-                                                    {imageFile ?
-                                                        <ImageViewer src={URL.createObjectURL(imageFile)} alt=""/>
-                                                        : <span
-                                                            className="m-8 block text-sm font-semibold text-gray-900">上傳完整拼圖</span>}
+                                                    密碼
                                                 </label>
                                                 <input
-                                                    id="file-upload"
-                                                    type="file"
-                                                    accept="image/*"
-                                                    className="hidden"
-                                                    onChange={handleInputFile}
+                                                    type="text"
+                                                    name="name"
+                                                    id="name"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                    onChange={(e) => setPassword(e.target.value)}
                                                 />
                                             </div>
                                         </div>
                                         <div className="mt-2">
                                             <p className="text-sm text-gray-500">
-                                                1. 上傳一張完整拼圖的照片，越清楚查找的效果越好。<br/>
-                                                2. 過大的提高解析度不一定會更清楚，反而可能讓查找效率降低。<br/>
-                                                3. 若拼圖片數過多，用拍攝的效果會不好，建議用掃描的方式。
+                                                Julo Julo Julo Julo Julo Julo Julo Julo Julo。
                                             </p>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                <div className="mt-5 sm:mt-8 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                                     <button
                                         type="button"
                                         className={`inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2
-                                                    ${(!checkCanUpload || uploadLoading) ? "bg-gray-500" : "bg-indigo-600 hover:bg-indigo-500"}`}
+                                                    ${(!checkInputOk || loading) ? "bg-gray-500" : "bg-indigo-600 hover:bg-indigo-500"}`}
                                         onClick={() => handleButtonClick(true)}
-                                        disabled={!checkCanUpload || uploadLoading}
+                                        disabled={!checkInputOk || loading}
                                     >
-                                        {uploadLoading && <LoadingSpinner/>}
+                                        {loading && <LoadingSpinner/>}
                                         確定
                                     </button>
                                     <button
                                         type="button"
                                         className={`mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:col-start-1 sm:mt-0
-                                                    ${uploadLoading ? "" : "hover:bg-gray-50"}`}
+                                                    ${loading ? "" : "hover:bg-gray-50"}`}
                                         onClick={() => handleButtonClick(false)}
-                                        disabled={uploadLoading}
+                                        disabled={loading}
                                     >
                                         取消
                                     </button>
